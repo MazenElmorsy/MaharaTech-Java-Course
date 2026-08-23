fce29718732197caa1046332696efe0e6327a7e9
Mazen Saeed <MazenElmorsy@outlook.com>
2026-08-23 17:42:27 +0300

    Learned basic console output and string literals with personal info

---
+++ src/Basics/Exercises/PersonalInfo.java
@@ -0,0 +1,10 @@
+package Basics.Exercises;
+
+public class PersonalInfo {
+    public static void main(String[] args){            //I Don't know what is this Yet :)
+        System.out.println("My name is: Mazen Saeed .");
+        System.out.println("I'm 20 years old.");
+        System.out.println("I'm a third-year Computer Engineer student in May University on Cairo.");
+        System.out.println("I love programming!");
+    }
+}
