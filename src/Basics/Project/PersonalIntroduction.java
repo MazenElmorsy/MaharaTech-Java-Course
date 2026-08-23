12e2a3922a6d79c49a5a845d9eb60aa39d2f70fb
Mazen Saeed <MazenElmorsy@outlook.com>
2026-08-23 17:47:07 +0300

    practiced variables, data types, string concatenation, and formatted output through a mini project

---
+++ src/Basics/Project/PersonalIntroduction.java
@@ -0,0 +1,16 @@
+package Basics.Project;
+
+public class PersonalIntroduction {
+    public static void main(String[] args){
+        String name = "Mazen Saeed";
+        int age = 20 ;
+        String goal = "Java Backend Developer/Cybersecurity Professional" ;
+        String university = "May University in Cairo" ;
+        String major = "Computer Engineer" ;
+        System.out.println("Name: "+name+".\n"+
+                           "Age: "+age+".\n"+
+                           "University: "+university+".\n"+
+                           "Major: "+major+".\n"+
+                           "Goal: "+goal+".");
+    }
+}
