6e1c39af9309365fe2fabe722d17f428bf4d0764
Mazen Saeed <MazenElmorsy@outlook.com>
2026-08-23 17:44:57 +0300

    Learned variables and arithmetic operators with a simple calculator

---
+++ src/Basics/Exercises/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package Basics.Exercises;
+
+public class SimpleCalculator {
+    public static void main(String[] args){
+        int firstNumber = 10 ;
+        int secondNumber = 2 ;
+
+        System.out.println("The addition of "+firstNumber+" and "+secondNumber+" is: "+ (firstNumber + secondNumber));
+        System.out.println("The subtraction of "+firstNumber+" and "+secondNumber+" is: "+ (firstNumber - secondNumber));
+        System.out.println("The multiplication of "+firstNumber+" and "+secondNumber+" is: "+ (firstNumber * secondNumber));
+        System.out.println("The division of "+firstNumber+" and "+secondNumber+" is: "+ (firstNumber / secondNumber));
+    }
+}
