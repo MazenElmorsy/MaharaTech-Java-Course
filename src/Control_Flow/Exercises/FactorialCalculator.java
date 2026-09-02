e21e9d15e39e736742c8179121cdc301acc7909a
Mazen Saeed <MazenElmorsy@outlook.com>
2026-09-02 14:55:47 +0300

    Made a factorial calculator using for loop and input handling

---
+++ src/Control_Flow/Exercises/FactorialCalculator.java
@@ -0,0 +1,27 @@
+package Control_Flow.Exercises;
+
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int number, result = 1;
+
+        System.out.print("Enter a number to get its factorial: ");
+        number = scanner.nextInt();
+
+        if(number < 0){
+            System.out.println("The number can't be negative!");
+        }
+        else{
+            for(int i = number; i > 0 ;i--){
+                result *= i ;
+            }
+        }
+
+        System.out.printf("The factorial of %d is %d.", number, result);
+
+        scanner.close();
+    }
+}
